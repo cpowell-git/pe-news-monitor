0c045a01c9229f4e08fc27b12645b7c0f733795a
cpowell-git <cpowell@rivercapital.com.au>
2026-03-24 16:24:38 +1100

    Update pe_news_monitor.py

--- pe_news_monitor.py
+++ pe_news_monitor.py
@@ -97,7 +97,7 @@ GOOGLE_NEWS_QUERIES = [
 ]
 
 SMTP_SERVER = os.getenv("SMTP_SERVER", "smtp.gmail.com")
-SMTP_PORT = int(os.getenv("SMTP_PORT", "587"))
+SMTP_PORT = int(os.getenv("SMTP_PORT") or "587")
 SMTP_USER = os.getenv("SMTP_USER")
 SMTP_PASS = os.getenv("SMTP_PASS")
 EMAIL_TO = os.getenv("EMAIL_TO")
